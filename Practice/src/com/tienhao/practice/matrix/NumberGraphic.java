5551e95ba256e4010516f19d1f805e69f0d3ab8c
張天皓 <tienhaochang703@gmail.com>
2018-08-03 04:54:24 +0800

    累

--- Practice/src/com/tienhao/practice/matrix/NumberGraphic.java
+++ Practice/src/com/tienhao/practice/matrix/NumberGraphic.java
@@ -55,21 +55,8 @@ public class NumberGraphic {
 		g.fillPolygon(poly1);
 	}
 	
-	static private void drawColorNumber(int[] lineColor, Color[] colorArray, int x, int y){
-		setHorizontalLine(55, 10, colorArray[lineColor[0]]);
-		setVerticalLine(10, 55, colorArray[lineColor[1]]);
-		setVerticalLine(100, 55, colorArray[lineColor[2]]);
-		setHorizontalLine(55, 100, colorArray[lineColor[3]]);
-		setVerticalLine(10, 145, colorArray[lineColor[4]]);
-		setVerticalLine(100, 145, colorArray[lineColor[5]]);
-		setHorizontalLine(55, 190, colorArray[lineColor[6]]);
-	}
-
-	static void print(String lineColorStr, Color[] colorArray, String fileName,int reverseCount) throws IOException {
-		BufferedImage bi = new BufferedImage(110, 200, BufferedImage.TYPE_INT_RGB);
-		g = bi.createGraphics();
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, 110, 200);
+	static private void drawColorNumber(String lineColorStr, Color[] colorArray, int x, int y){
+		
 		int[] lineColor = {Integer.parseInt(lineColorStr.substring(0, 1)),
 				Integer.parseInt(lineColorStr.substring(1, 2)),
 				Integer.parseInt(lineColorStr.substring(2, 3)),
@@ -77,7 +64,22 @@ public class NumberGraphic {
 				Integer.parseInt(lineColorStr.substring(4, 5)),
 				Integer.parseInt(lineColorStr.substring(5, 6)),
 				Integer.parseInt(lineColorStr.substring(6, 7)),};
-		drawColorNumber(lineColor,colorArray,0,0);
+		
+		setHorizontalLine(x+55, y+10, colorArray[lineColor[0]]);
+		setVerticalLine(x+10, y+55, colorArray[lineColor[1]]);
+		setVerticalLine(x+100, y+55, colorArray[lineColor[2]]);
+		setHorizontalLine(x+55, y+100, colorArray[lineColor[3]]);
+		setVerticalLine(x+10, y+145, colorArray[lineColor[4]]);
+		setVerticalLine(x+100, y+145, colorArray[lineColor[5]]);
+		setHorizontalLine(x+55, y+190, colorArray[lineColor[6]]);
+	}
+
+	static void print(String lineColorStr, Color[] colorArray, String fileName,int reverseCount) throws IOException {
+		BufferedImage bi = new BufferedImage(110, 200, BufferedImage.TYPE_INT_RGB);
+		g = bi.createGraphics();
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, 110, 200);
+		drawColorNumber(lineColorStr,colorArray,0,0);
 		
 		g.dispose();
 
@@ -86,13 +88,38 @@ public class NumberGraphic {
 
 	}
 	
-	static void printNumbers(List<int[]> lineColor, Color[] colorArray, String fileName,int reverseCount) throws IOException {
-		BufferedImage bi = new BufferedImage(110+130*lineColor.size(), 200, BufferedImage.TYPE_INT_RGB);
+	private static void printNumbers(String[] lineColor, Color[] colorArray, String fileName,int reverseCount) throws IOException {
+		BufferedImage bi = new BufferedImage(110+130*(lineColor.length-1), 200, BufferedImage.TYPE_INT_RGB);
 		g = bi.createGraphics();
 		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, 130*lineColor.size(), 200);
-		for(int i=0;i<lineColor.size();i++){			
-			drawColorNumber(lineColor.get(i),colorArray,130*i,0);
+		g.fillRect(0, 0,110+130*(lineColor.length-1), 200);
+		for(int i=0;i<lineColor.length;i++){			
+			drawColorNumber(lineColor[i],colorArray,130*i,0);
+		}
+		
+		g.dispose();
+
+		File file = new File("D://Graphics/" +reverseCount+File.separator+ fileName + ".png");
+		ImageIO.write(bi, "png", file);
+
+	}
+	
+	private static void printListNumbers(String[][] lineColor, Color[] colorArray, String fileName,int reverseCount) throws IOException {
+		BufferedImage bi = new BufferedImage(2000, 2800, BufferedImage.TYPE_INT_RGB);
+		g = bi.createGraphics();
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0,2000, 2800);
+		int xa=50;
+		int ya=20;
+		for(int i=0;i<lineColor.length;i++){
+			for(int j=0;j<lineColor[i].length;j++){
+				drawColorNumber(lineColor[i][j],colorArray,xa+130*j,ya);
+			}
+			ya = ya + 230;
+			if(i%12==0){
+				ya = 20;
+				xa=xa+500;
+			}
 		}
 		
 		g.dispose();
@@ -106,7 +133,42 @@ public class NumberGraphic {
 //		Color[] color = { Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW };
 //		int[] lineColor = { 0, 2, 3, 1, 0, 2, 1 };
 //		print(lineColor, color, "0231021");
-		printAll();
+//		printAll();
+//		printNumbers(new String[]{"1012202","0232212","1013010"},new Color[]{ Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW},"test",5);
+		printListNumbers(new String[][]{
+				{"1012202","0232212","1013010"},
+				{"0100212","1232202","0103101"},
+				{"0201121","1323323","0121323"},
+				{"0103101","1232202","0100212"},
+				{"1231231","1011202","1232202"},
+				{"0023323","0203121","1121323"},
+				{"1121321","3121323","1213020"},
+				{"0020320","3020323","1123323"},
+				{"0200121","0230230","1012202"},
+				{"2122010","0320320","0203121"},
+				{"0121323","0023020","2022321"},
+				{"1323323","0023020","1121323"},
+				
+				{"0023323","2122320","2022101"},
+				{"0020323","1211020","1011202"},
+				{"0121323","1020323","0201121"},
+				{"0323323","1020323","2021101"},
+				{"1211020","0232212","1232202"},
+				{"0200121","0232212","1232202"},
+				{"0103101","1123121","0203121"},
+				{"0103101","0023020","1123121"},
+				
+				{"1121321","2022101","1123323"},
+				{"0020320","0100212","0203121"},
+				{"1121321","3121323","2120010"},
+				{"2122320","3020323","1123323"},
+				{"0023020","1011202","2022322"},
+				{"2232212","1211020","3230200"},
+				{"1011202","1232202","3231211"},
+				{"2022101","0121323","2122322"},
+				{"0023323","2021101","0103101"},
+				{"1231231","3230200","2021101"}
+				},new Color[]{ Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW},"test",5);
 	}
 	
 	private static void printAll() throws IOException {
