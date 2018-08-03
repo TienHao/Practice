095b52d3acee22f90f21f2cd4e8435981dc9524e
1701014 <1701014@twdann1701014.hq.intra.acer.com>
2018-08-03 19:55:15 +0800

    累2

--- Practice/src/com/tienhao/practice/matrix/NumberGraphic.java
+++ Practice/src/com/tienhao/practice/matrix/NumberGraphic.java
@@ -7,7 +7,9 @@ import java.awt.Polygon;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -103,22 +105,29 @@ public class NumberGraphic {
 		ImageIO.write(bi, "png", file);
 
 	}
+
 	
 	private static void printListNumbers(String[][] lineColor, Color[] colorArray, String fileName,int reverseCount) throws IOException {
-		BufferedImage bi = new BufferedImage(2000, 2800, BufferedImage.TYPE_INT_RGB);
+		BufferedImage bi = new BufferedImage(2800, 2000, BufferedImage.TYPE_INT_RGB);
 		g = bi.createGraphics();
 		g.setColor(Color.WHITE);
-		g.fillRect(0, 0,2000, 2800);
-		int xa=50;
+		g.fillRect(0, 0,2800, 2000);
+		int xa=60;
 		int ya=20;
+		Map<String,Color> color = new HashMap<String,Color>();
+		color.put("1", Color.RED);
+		color.put("2", Color.GREEN);
+		color.put("3", Color.BLUE);
+		color.put("4", Color.YELLOW);
 		for(int i=0;i<lineColor.length;i++){
-			for(int j=0;j<lineColor[i].length;j++){
+			for(int j=0;j<3;j++){
 				drawColorNumber(lineColor[i][j],colorArray,xa+130*j,ya);
 			}
-			ya = ya + 230;
-			if(i%12==0){
+			drawCelleophoneColor(color.get(lineColor[i][3]),xa-30,ya+100);
+			ya = ya + 250;
+			if((i+1)%8==0){
 				ya = 20;
-				xa=xa+500;
+				xa=xa+470;
 			}
 		}
 		
@@ -129,6 +138,10 @@ public class NumberGraphic {
 
 	}
 
+	private static void drawCelleophoneColor(Color color, int i, int ya) {
+		setVerticalLine(i,ya, color);
+	}
+
 	public static void main(String[] args) throws IOException {
 //		Color[] color = { Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW };
 //		int[] lineColor = { 0, 2, 3, 1, 0, 2, 1 };
@@ -136,39 +149,39 @@ public class NumberGraphic {
 //		printAll();
 //		printNumbers(new String[]{"1012202","0232212","1013010"},new Color[]{ Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW},"test",5);
 		printListNumbers(new String[][]{
-				{"1012202","0232212","1013010"},
-				{"0100212","1232202","0103101"},
-				{"0201121","1323323","0121323"},
-				{"0103101","1232202","0100212"},
-				{"1231231","1011202","1232202"},
-				{"0023323","0203121","1121323"},
-				{"1121321","3121323","1213020"},
-				{"0020320","3020323","1123323"},
-				{"0200121","0230230","1012202"},
-				{"2122010","0320320","0203121"},
-				{"0121323","0023020","2022321"},
-				{"1323323","0023020","1121323"},
+				{"1012202","0232212","1013010","2"},
+				{"0100212","1232202","0103101","1"},
+				{"0201121","1323323","0121323","3"},
+				{"0103101","1232202","0100212","4"},
+				{"1231231","1011202","1232202","3"},
+				{"0023323","0203121","1121323","1"},
+				{"1121321","3121323","1213020","4"},
+				{"0020320","3020323","1123323","1"},
+				{"0200121","0230230","1012202","2"},
+				{"2122010","0320320","0203121","3"},
+				{"0121323","0023020","2022321","4"},
+				{"1323323","0023020","1121323","2"},
 				
-				{"0023323","2122320","2022101"},
-				{"0020323","1211020","1011202"},
-				{"0121323","1020323","0201121"},
-				{"0323323","1020323","2021101"},
-				{"1211020","0232212","1232202"},
-				{"0200121","0232212","1232202"},
-				{"0103101","1123121","0203121"},
-				{"0103101","0023020","1123121"},
+				{"0023323","2122320","2022101","1"},
+				{"0020323","1211020","1011202","1"},
+				{"0121323","1020323","0201121","2"},
+				{"0323323","1020323","2021101","2"},
+				{"1211020","0232212","1232202","3"},
+				{"0200121","0232212","1232202","3"},
+				{"0103101","1123121","0203121","4"},
+				{"0103101","0023020","1123121","4"},
 				
-				{"1121321","2022101","1123323"},
-				{"0020320","0100212","0203121"},
-				{"1121321","3121323","2120010"},
-				{"2122320","3020323","1123323"},
-				{"0023020","1011202","2022322"},
-				{"2232212","1211020","3230200"},
-				{"1011202","1232202","3231211"},
-				{"2022101","0121323","2122322"},
-				{"0023323","2021101","0103101"},
-				{"1231231","3230200","2021101"}
-				},new Color[]{ Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW},"test",5);
+				{"1121321","2022101","1123323","2"},
+				{"0020320","0100212","0203121","1"},
+				{"1121321","3121323","2120010","4"},
+				{"2122320","3020323","1123323","1"},
+				{"0023020","1011202","2022322","3"},
+				{"2232212","1211020","3230200","1"},
+				{"1011202","1232202","3231211","3"},
+				{"2022101","0121323","2122322","2"},
+				{"0023323","2021101","0103101","4"},
+				{"1231231","3230200","2021101","3"}
+				},new Color[]{ new Color(255,75,75), new Color(195,255,0 ), new Color(0,195,255), new Color(255,255,0)},"test",5);
 	}
 	
 	private static void printAll() throws IOException {
